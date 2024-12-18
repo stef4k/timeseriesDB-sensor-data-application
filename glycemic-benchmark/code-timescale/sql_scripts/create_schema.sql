dfe321ea4d26ae8a1211a3151ec3ba2b42c2cfd5
Nishant Sushmakar <nishantsushmakar@Nishants-MacBook-Pro.local>
2024-12-18 13:41:29 +0100

    Compatibility Fix for 2.6.1

--- glycemic-benchmark/code-timescale/sql_scripts/create_schema.sql
+++ glycemic-benchmark/code-timescale/sql_scripts/create_schema.sql
@@ -24,7 +24,7 @@ CREATE TABLE accelerometer_data  (
 	acc_z	NUMERIC,
 	participant_id INTEGER REFERENCES demographics(id)
 );
-SELECT create_hypertable('accelerometer_data', by_range('ts', INTERVAL '1 day'));
+SELECT create_hypertable('accelerometer_data','ts',chunk_time_interval => INTERVAL '1 day');
 ALTER TABLE accelerometer_data SET (timescaledb.compress, timescaledb.compress_orderby='ts');
 SELECT add_compression_policy('accelerometer_data', INTERVAL '2 weeks');
 
@@ -33,7 +33,7 @@ CREATE TABLE blood_volume_pulse (
 	bvp	NUMERIC,
 	participant_id INTEGER REFERENCES demographics(id)
 );
-SELECT create_hypertable('blood_volume_pulse', by_range('ts', INTERVAL '1 day'));
+SELECT create_hypertable('blood_volume_pulse','ts',chunk_time_interval => INTERVAL '1 day');
 ALTER TABLE blood_volume_pulse SET (timescaledb.compress, timescaledb.compress_orderby='ts');
 SELECT add_compression_policy('blood_volume_pulse', INTERVAL '2 weeks');
 
@@ -53,7 +53,7 @@ CREATE TABLE interstitial_glucose  (
 	transmitter_time NUMERIC,
 	participant_id INTEGER REFERENCES demographics(id)
 );
-SELECT create_hypertable('interstitial_glucose',by_range('ts', INTERVAL '1 day'));
+SELECT create_hypertable('interstitial_glucose','ts',chunk_time_interval => INTERVAL '1 day');
 ALTER TABLE interstitial_glucose SET(timescaledb.compress, timescaledb.compress_orderby='ts');
 SELECT add_compression_policy('interstitial_glucose', INTERVAL '2 weeks');
 
@@ -63,7 +63,7 @@ CREATE TABLE electrodermal_activity(
 	eda	numeric,
 	participant_id INTEGER REFERENCES demographics(id)
 );
-SELECT create_hypertable('electrodermal_activity', by_range('ts', INTERVAL '1 day'));
+SELECT create_hypertable('electrodermal_activity','ts',chunk_time_interval => INTERVAL '1 day');
 ALTER TABLE electrodermal_activity SET (timescaledb.compress, timescaledb.compress_orderby='ts');
 SELECT add_compression_policy('electrodermal_activity', INTERVAL '2 weeks');
 
@@ -72,7 +72,7 @@ CREATE TABLE heart_rate_data (
     hr DECIMAL(5, 2),
 	participant_id INTEGER REFERENCES demographics(id)
 );
-SELECT create_hypertable('heart_rate_data',by_range('datetime', INTERVAL '1 day'));
+SELECT create_hypertable('heart_rate_data','datetime',chunk_time_interval => INTERVAL '1 day');
 ALTER TABLE heart_rate_data SET (timescaledb.compress, timescaledb.compress_orderby='datetime');
 SELECT add_compression_policy('heart_rate_data', INTERVAL '2 weeks');
 
@@ -82,7 +82,7 @@ CREATE TABLE ibi_data (
     ibi NUMERIC(10, 6),       -- Allows up to 10 digits with 6 decimal places for precision
 	participant_id INTEGER REFERENCES demographics(id)
 );
-SELECT create_hypertable('ibi_data',by_range('event_time', INTERVAL '1 day'));
+SELECT create_hypertable('ibi_data','event_time',chunk_time_interval => INTERVAL '1 day');
 ALTER TABLE ibi_data SET (timescaledb.compress, timescaledb.compress_orderby='event_time');
 SELECT add_compression_policy('ibi_data', INTERVAL '2 weeks');
 
@@ -91,6 +91,6 @@ CREATE TABLE temperature_data (
     temp NUMERIC(5, 2) ,      -- Allows up to 5 digits with 2 decimal places for precision (e.g., 999.99)
 	participant_id INTEGER REFERENCES demographics(id)
 );
-SELECT create_hypertable('temperature_data', by_range('event_time', INTERVAL '1 day'));
+SELECT create_hypertable('temperature_data','event_time',chunk_time_interval => INTERVAL '1 day');
 ALTER TABLE temperature_data SET (timescaledb.compress, timescaledb.compress_orderby='event_time');
 SELECT add_compression_policy('temperature_data', INTERVAL '2 weeks');
