7b6b5a7e4467c48903ced69458594bf8988bd696
Olha <99298961+kvitka-dykogo-granatu@users.noreply.github.com>
2024-12-19 19:14:01 +0100

    InfluxDB custom benchmark

--- glycemic-benchmark/code-influx/config.py
+++ glycemic-benchmark/code-influx/config.py
@@ -1,12 +1,12 @@
 
-SCALE_FACTOR = 2
-NUMBER_TIMES_TO_RUN_QUERY = 3
-DATA_PATH = "../data/"
-TRANSFORM_DATA_PATH = "../new_data/"
+SCALE_FACTOR = 5
+NUMBER_TIMES_TO_RUN_QUERY = 5
+DATA_PATH = "../../data/"
+TRANSFORM_DATA_PATH = "../../new_data/"
 DB_NAME = "benchmark_db"
 DB_USER = "postgres"
 DB_PORT = "8086"
 DB_HOST = "localhost"
 DB_PASSWORD = "password"
-RESULTS_PATH = "../results/"
-SQL_SCRIPTS_PATH = "sql_scripts/"
+RESULTS_PATH = "../../results/"
+QUERIES_PATH = "queries/"
